a6147c168ce1a829ed42f3eae121c1762dcf391f
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-08-17 14:09:17 +0530

    Day 01: Q1 and Q2

---
+++ Day-01/Q1.c
@@ -0,0 +1,8 @@
+//
+// Created by Aarav Choudhary on 17/08/26.
+//
+#include<stdio.h>
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ Day-01/Q2.c
@@ -0,0 +1,8 @@
+//
+// Created by Aarav Choudhary on 17/08/26.
+//
+#include <stdio.h>
+int main (void) {
+    printf("Welcome to Day 1 of 100 Days of C!\n");
+    return 0;
+}
