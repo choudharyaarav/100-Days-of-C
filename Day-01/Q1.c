48cc78f8ff9bf553ce235ade7d29bcc78e7da88f
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-08-17 16:18:39 +0530

    Day 01:Q1 and Q2 solutions

--- Day-01/Q1.c
+++ Day-01/Q1.c
@@ -1,8 +1,25 @@
-//
-// Created by Aarav Choudhary on 17/08/26.
-//
-#include<stdio.h>
-int main(void) {
-    printf("Hello World!\n");
+// Created by Aarav Choudhary on 10/08/26.
+//Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include <stdio.h>
+int main () {
+    int a,b,sum;
+    printf("Enter the value of a and b");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("sum=%d",sum);
     return 0;
 }
