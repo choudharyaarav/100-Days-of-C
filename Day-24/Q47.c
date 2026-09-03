3520cdb20f533c1c06b068ee4ac32b08c85fa27c
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-04 04:11:10 +0530

    Day 24: Q47 and Q48 solutions

--- Day-24/Q47.c
+++ Day-24/Q47.c
@@ -1,3 +1,31 @@
-//
-// Created by Aarav Choudhary on 04/09/26.
-//
+// Created by Aarav Choudhary on 02/09/26.
+/* Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
--- Day-24/Q48.c
+++ Day-24/Q48.c
@@ -1,3 +1,31 @@
-//
-// Created by Aarav Choudhary on 04/09/26.
-//
+// Created by Aarav Choudhary on 02/09/26.
+/* Q48: Write a program to print the following pattern:
+1
+12
+123
+1234
+12345
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+1
+12
+123
+1234
+12345
+
+*/
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
