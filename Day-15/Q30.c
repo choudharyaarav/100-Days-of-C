3b8c3504ad9898caf5db244e8c1de82878ece0b3
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-04 04:11:09 +0530

    Day 15: Q29 and Q30 solutions

--- Day-15/Q30.c
+++ Day-15/Q30.c
@@ -1,3 +1,33 @@
-//
-// Created by Aarav Choudhary on 04/09/26.
-//
+// Created by Aarav Choudhary on 24/08/26.
+//Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, reverse = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+
+    printf("%d", reverse);
+
+    return 0;
+}
