2fab4d3c8496969e9ff1a30b29270a1397e12d22
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-04 04:11:10 +0530

    Day 25: Q49 and Q50 solutions

--- Day-25/Q49.c
+++ Day-25/Q49.c
@@ -1,3 +1,32 @@
-//
-// Created by Aarav Choudhary on 04/09/26.
-//
+// Created by Aarav Choudhary on 03/09/26.
+/* Q49: Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+5
+45
+345
+2345
+12345
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = n - i + 1; j <= n; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
