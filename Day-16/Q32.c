51bce8bc62948f0a5fc69516fb68669058009eb9
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-04 04:11:10 +0530

    Day 16: Q31 and Q32 solutions

--- Day-16/Q32.c
+++ Day-16/Q32.c
@@ -1,3 +1,37 @@
-//
-// Created by Aarav Choudhary on 04/09/26.
-//
+// Created by Aarav Choudhary on 25/08/26.
+//Q32: Write a program to check if a number is a palindrome.
+
+/*
+Sample Test Cases:
+Input 1:
+121
+Output 1:
+Palindrome
+
+Input 2:
+123
+Output 2:
+Not palindrome
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, original, reverse = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
