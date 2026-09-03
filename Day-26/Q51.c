9f39858f7d566d1d29e54a97f2fd85aaf3cee67d
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-04 04:11:10 +0530

    Day 26: Q51 and Q52 solutions

--- Day-26/Q51.c
+++ Day-26/Q51.c
@@ -1,3 +1,35 @@
-//
 // Created by Aarav Choudhary on 04/09/26.
-//
+/* Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+ 12345
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            printf(" ");
+        }
+        for (int j = n - i + 1; j <= n; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
--- Day-26/Q52.c
+++ Day-26/Q52.c
@@ -1,3 +1,43 @@
-//
 // Created by Aarav Choudhary on 04/09/26.
-//
+/* Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+#include <stdio.h>
+
+int main() {
+    int counts[] = {1, 3, 5, 3, 1};
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < counts[i]; j++) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
