397e4435b4aed4d108760fce1616dd36b6361650
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-04 04:11:09 +0530

    Day 13: Q25 and Q26 solutions

--- Day-13/Q25.c
+++ Day-13/Q25.c
@@ -1,3 +1,55 @@
-//
-// Created by Aarav Choudhary on 04/09/26.
-//
+// Created by Aarav Choudhary on 22/08/26.
+//Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+/*
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char op;
+    printf("Enter expression (num1 num2 operator): ");
+    scanf("%f %f %c", &num1, &num2, &op);
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%g", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%g", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%g", result);
+            break;
+        case '/':
+            result = num1 / num2;
+            printf("%g", result);
+            break;
+        case '%':
+            printf("%d", (int)num1 % (int)num2);
+            break;
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
