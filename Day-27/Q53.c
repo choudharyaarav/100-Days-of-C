efd72b2e88581827300ea8c3d1cfc2b1850471e0
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-05 19:43:19 +0530

    Day 27:Q53 and Q54 solutions

--- Day-27/Q53.c
+++ Day-27/Q53.c
@@ -1,3 +1,50 @@
-//
 // Created by Aarav Choudhary on 05/09/26.
-//
+/* Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n = 5, i, j, stars;
+
+    // Upper half (including middle row)
+    for (i = 1; i <= n; i++) {
+        stars = 2 * i - 1;
+        for (j = 1; j <= stars; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    // Lower half
+    for (i = n - 1; i >= 1; i--) {
+        stars = 2 * i - 1;
+        for (j = 1; j <= stars; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
--- Day-27/Q54.c
+++ Day-27/Q54.c
@@ -1,3 +1,49 @@
-//
 // Created by Aarav Choudhary on 05/09/26.
-//
+/* Q54: Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include <stdio.h>
+
+int main() {
+ int n = 4, i, j, spaces, stars;
+
+ // Upper half
+ for (i = 1; i <= n; i++) {
+  spaces = n - i;
+  stars = 2 * i - 1;
+
+  for (j = 1; j <= spaces; j++)
+   printf(" ");
+  for (j = 1; j <= stars; j++)
+   printf("*");
+  printf("\n");
+ }
+
+ // Lower half
+ for (i = n - 1; i >= 1; i--) {
+  spaces = n - i;
+  stars = 2 * i - 1;
+
+  for (j = 1; j <= spaces; j++)
+   printf(" ");
+  for (j = 1; j <= stars; j++)
+   printf("*");
+  printf("\n");
+ }
+
+ return 0;
+}
