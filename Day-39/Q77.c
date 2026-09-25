7bac1269c705515bae47506c207a97ee5a948df0
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:37 +0530

    Day 39: Q77 and Q78 solutions

--- Day-39/Q77.c
+++ Day-39/Q77.c
@@ -1,3 +1,45 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 17/09/26.
+/* Q77: Check if the elements on the diagonal of a matrix are distinct.
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 1
+Output 1:
+False
+
+Input 2:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 2:
+True
+*/
+#include <stdio.h>
+
+int main() {
+    int n, dummy, i, j, mat[20][20], distinct = 1;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &n, &dummy);
+
+    printf("Enter %d elements: ", n * n);
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &mat[i][j]);
+
+    for (i = 0; i < n; i++)
+        for (j = i + 1; j < n; j++)
+            if (mat[i][i] == mat[j][j])
+                distinct = 0;
+
+    if (distinct)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
--- Day-39/Q78.c
+++ Day-39/Q78.c
@@ -1,3 +1,32 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 17/09/26.
+/* Q78: Find the sum of main diagonal elements for a square matrix.
+
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+15
+*/
+#include <stdio.h>
+
+int main() {
+    int n, dummy, i, j, mat[20][20], sum = 0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &n, &dummy);
+
+    printf("Enter %d elements: ", n * n);
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &mat[i][j]);
+
+    for (i = 0; i < n; i++)
+        sum += mat[i][i];
+
+    printf("Diagonal sum = %d", sum);
+
+    return 0;
+}
