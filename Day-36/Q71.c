d355d456214dc1883970db6aa03c468f4c326eca
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:30 +0530

    Day 36: Q71 and Q72 solutions

--- Day-36/Q71.c
+++ Day-36/Q71.c
@@ -1,3 +1,34 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 14/09/26.
+/* Q71: Read and print a matrix.
+
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+*/
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j, mat[20][20];
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter %d elements: ", r * c);
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            scanf("%d", &mat[i][j]);
+
+    printf("Matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++)
+            printf("%d ", mat[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
--- Day-36/Q72.c
+++ Day-36/Q72.c
@@ -1,3 +1,32 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 14/09/26.
+/* Q72: Find the sum of all elements in a matrix.
+
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+21
+*/
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j, mat[20][20], sum = 0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter %d elements: ", r * c);
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            scanf("%d", &mat[i][j]);
+
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            sum += mat[i][j];
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
