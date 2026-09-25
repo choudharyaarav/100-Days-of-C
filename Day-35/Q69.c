8c3f06592bda270705e55a35ac7f390b319ef509
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:28 +0530

    Day 35: Q69 and Q70 solutions

--- Day-35/Q69.c
+++ Day-35/Q69.c
@@ -1,3 +1,36 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 13/09/26.
+/* Q69: Find the second largest element in an array.
+
+Sample Test Cases:
+Input 1:
+5
+10 20 30 40 50
+Output 1:
+40
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], first, second;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    first = second = -2147483647;
+    for (i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second largest = %d", second);
+
+    return 0;
+}
--- Day-35/Q70.c
+++ Day-35/Q70.c
@@ -1,3 +1,37 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 13/09/26.
+/* Q70: Rotate an array to the right by k positions.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+4 5 1 2 3
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], temp[100], k;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for (i = 0; i < n; i++)
+        temp[(i + k) % n] = arr[i];
+
+    printf("Rotated array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", temp[i]);
+
+    return 0;
+}
