8081c93c7a987338981945c5cf92f6ec9ad1e026
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:46 +0530

    Day 43: Q85 and Q86 solutions

--- Day-43/Q85.c
+++ Day-43/Q85.c
@@ -1,3 +1,27 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 21/09/26.
+/* Q85: Reverse a string.
+
+Sample Test Cases:
+Input 1:
+abcd
+Output 1:
+dcba
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, len = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[len] != '\0')
+        len++;
+
+    printf("Reversed: ");
+    for (i = len - 1; i >= 0; i--)
+        printf("%c", str[i]);
+
+    return 0;
+}
--- Day-43/Q86.c
+++ Day-43/Q86.c
@@ -1,3 +1,40 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 21/09/26.
+/* Q86: Check if a string is a palindrome.
+
+Sample Test Cases:
+Input 1:
+madam
+Output 1:
+Palindrome
+
+Input 2:
+hello
+Output 2:
+Not palindrome
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, len = 0, isPalindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[len] != '\0')
+        len++;
+
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
