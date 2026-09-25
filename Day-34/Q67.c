e3d5414fa18d3d32211f2a9335973fdba25f2143
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:26 +0530

    Day 34: Q67 and Q68 solutions

--- Day-34/Q67.c
+++ Day-34/Q67.c
@@ -1,3 +1,37 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 12/09/26.
+/* Q67: Insert an element in an array at a given position.
+
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], pos, value;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter position and value to insert: ");
+    scanf("%d %d", &pos, &value);
+
+    for (i = n; i > pos; i--)
+        arr[i] = arr[i - 1];
+    arr[pos] = value;
+    n++;
+
+    printf("Array after insertion: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
--- Day-34/Q68.c
+++ Day-34/Q68.c
@@ -1,3 +1,36 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 12/09/26.
+/* Q68: Delete an element from an array.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+1 2 4 5
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    for (i = pos; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    n--;
+
+    printf("Array after deletion: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
