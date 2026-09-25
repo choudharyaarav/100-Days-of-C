d1319a8d97b77b38dddd368fc9577fb17ea7afcc
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:57 +0530

    Day 48: Q95 and Q96 solutions

--- Day-48/Q95.c
+++ Day-48/Q95.c
@@ -1,3 +1,43 @@
-//
 // Created by Aarav Choudhary on 26/09/26.
-//
+/* Q95: Check if one string is a rotation of another.
+
+Sample Test Cases:
+Input 1:
+abcde
+deabc
+Output 1:
+Rotation
+
+Input 2:
+abc
+acb
+Output 2:
+Not rotation
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], combined[200];
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    if (strlen(str1) != strlen(str2)) {
+        printf("Not rotation");
+        return 0;
+    }
+
+    strcpy(combined, str1);
+    strcat(combined, str1);
+
+    if (strstr(combined, str2) != NULL)
+        printf("Rotation");
+    else
+        printf("Not rotation");
+
+    return 0;
+}
--- Day-48/Q96.c
+++ Day-48/Q96.c
@@ -1,3 +1,34 @@
-//
 // Created by Aarav Choudhary on 26/09/26.
-//
+/* Q96: Reverse each word in a sentence without changing the word order.
+
+Sample Test Cases:
+Input 1:
+I love coding
+Output 1:
+I evol gnidoc
+*/
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i = 0, start = 0, end;
+
+    printf("Enter a sentence: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        if (str[i] == ' ' || str[i + 1] == '\0') {
+            end = (str[i] == ' ') ? i - 1 : i;
+            while (end >= start) {
+                printf("%c", str[end]);
+                end--;
+            }
+            if (str[i] == ' ')
+                printf(" ");
+            start = i + 1;
+        }
+        i++;
+    }
+
+    return 0;
+}
