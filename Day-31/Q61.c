07eea7a51b71b20ee15a692961d5a8aa94c14702
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:33:28 +0530

    Day 31: Q61 and Q62 solutions

--- Day-31/Q61.c
+++ Day-31/Q61.c
@@ -1,3 +1,46 @@
-//
-// Created by Aarav Choudhary on 10/09/26.
-//
+// Created by Aarav Choudhary on 09/09/26.
+/* Q61: Search for an element in an array using linear search.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+3
+Output 1:
+Found at index 2
+
+Input 2:
+4
+10 20 30 40
+25
+Output 2:
+-1
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], key, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            printf("Found at index %d", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+        printf("-1");
+
+    return 0;
+}
--- Day-31/Q62.c
+++ Day-31/Q62.c
@@ -1,3 +1,38 @@
-//
-// Created by Aarav Choudhary on 10/09/26.
-//
+// Created by Aarav Choudhary on 09/09/26.
+/* Q62: Reverse an array without taking extra space.
+
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], temp, start, end;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    start = 0;
+    end = n - 1;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    printf("Reversed array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
