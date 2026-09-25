912dd5bd71f3bb62d6e03275a5a9f7bba41f8dfb
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:47:01 +0530

    Day 50: Q99 and Q100 solutions

--- Day-50/Q100.c
+++ Day-50/Q100.c
@@ -1,3 +1,33 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 28/09/26.
+/* Q100: Print all sub-strings of a string.
+
+Sample Test Cases:
+Input 1:
+abc
+Output 1:
+a,ab,abc,b,bc,c
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, k, len = 0, first = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[len] != '\0')
+        len++;
+
+    for (i = 0; i < len; i++) {
+        for (j = i; j < len; j++) {
+            if (!first)
+                printf(",");
+            for (k = i; k <= j; k++)
+                printf("%c", str[k]);
+            first = 0;
+        }
+    }
+
+    return 0;
+}
--- Day-50/Q99.c
+++ Day-50/Q99.c
@@ -1,3 +1,40 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 28/09/26.
+/* Q99: Change the date format from dd/04/yyyy to dd-Apr-yyyy.
+
+Sample Test Cases:
+Input 1:
+15/04/2025
+Output 1:
+15-Apr-2025
+*/
+#include <stdio.h>
+
+int main() {
+    char date[20], day[3], month[3], year[5];
+    char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    int monthNum;
+
+    printf("Enter date (dd/mm/yyyy): ");
+    scanf("%s", date);
+
+    day[0] = date[0];
+    day[1] = date[1];
+    day[2] = '\0';
+
+    month[0] = date[3];
+    month[1] = date[4];
+    month[2] = '\0';
+
+    year[0] = date[6];
+    year[1] = date[7];
+    year[2] = date[8];
+    year[3] = date[9];
+    year[4] = '\0';
+
+    monthNum = (month[0] - '0') * 10 + (month[1] - '0');
+
+    printf("%s-%s-%s", day, months[monthNum - 1], year);
+
+    return 0;
+}
