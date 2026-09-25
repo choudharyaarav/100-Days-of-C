9fe94c2eae49ae45dfdf7b3ca37d885b3e19eedc
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:53 +0530

    Day 46: Q91 and Q92 solutions

--- Day-46/Q91.c
+++ Day-46/Q91.c
@@ -1,3 +1,33 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 24/09/26.
+/* Q91: Remove all vowels from a string.
+
+Sample Test Cases:
+Input 1:
+education
+Output 1:
+dctn
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, j = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        char ch = str[i];
+        if (!(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+              ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')) {
+            str[j] = str[i];
+            j++;
+              }
+        i++;
+    }
+    str[j] = '\0';
+
+    printf("Result: %s", str);
+
+    return 0;
+}
--- Day-46/Q92.c
+++ Day-46/Q92.c
@@ -1,3 +1,36 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 24/09/26.
+/* Q92: Find the first repeating lowercase alphabet in a string.
+
+Sample Test Cases:
+Input 1:
+stress
+Output 1:
+s
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count[26] = {0};
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            count[str[i] - 'a']++;
+        i++;
+    }
+
+    i = 0;
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z' && count[str[i] - 'a'] > 1) {
+            printf("%c", str[i]);
+            break;
+        }
+        i++;
+    }
+
+    return 0;
+}
