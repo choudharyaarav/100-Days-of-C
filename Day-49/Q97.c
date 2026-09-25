2506065549495298f00830da1db54312fad68e57
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:59 +0530

    Day 49: Q97 and Q98 solutions

--- Day-49/Q97.c
+++ Day-49/Q97.c
@@ -1,3 +1,28 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 27/09/26.
+/* Q97: Print the initials of a name.
+
+Sample Test Cases:
+Input 1:
+John Doe
+Output 1:
+J.D.
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter full name: ");
+    scanf("%[^\n]", str);
+
+    printf("%c.", str[0]);
+
+    while (str[i] != '\0') {
+        if (str[i] == ' ' && str[i + 1] != '\0')
+            printf("%c.", str[i + 1]);
+        i++;
+    }
+
+    return 0;
+}
--- Day-49/Q98.c
+++ Day-49/Q98.c
@@ -1,3 +1,39 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 27/09/26.
+/* Q98: Print initials of a name with the surname displayed in full.
+
+Sample Test Cases:
+Input 1:
+John David Doe
+Output 1:
+J.D. Doe
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100], words[10][20];
+    int i = 0, j = 0, k, wordCount = 0;
+
+    printf("Enter full name: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        if (str[i] == ' ') {
+            words[wordCount][j] = '\0';
+            wordCount++;
+            j = 0;
+        } else {
+            words[wordCount][j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    words[wordCount][j] = '\0';
+    wordCount++;
+
+    for (k = 0; k < wordCount - 1; k++)
+        printf("%c.", words[k][0]);
+
+    printf(" %s", words[wordCount - 1]);
+
+    return 0;
+}
