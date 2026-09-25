71b9cafd2ee0545697213ed7346754d7b5f06f1e
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:23 +0530

    Day 33: Q65 and Q66 solutions

--- Day-33/Q65.c
+++ Day-33/Q65.c
@@ -1,3 +1,54 @@
-//
 // Created by Aarav Choudhary on 11/09/26.
-//
+/* Q65: Search in a sorted array using binary search.
+
+Sample Test Cases:
+Input 1:
+5
+1 3 5 7 9
+7
+Output 1:
+Found at index 3
+
+Input 2:
+5
+1 3 5 7 9
+6
+Output 2:
+-1
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], key, low, high, mid, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            printf("Found at index %d", mid);
+            found = 1;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found)
+        printf("-1");
+
+    return 0;
+}
--- Day-33/Q66.c
+++ Day-33/Q66.c
@@ -1,3 +1,40 @@
-//
 // Created by Aarav Choudhary on 11/09/26.
-//
+/* Q66: Insert an element in a sorted array at the appropriate position.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 4 5 6
+3
+Output 1:
+1 2 3 4 5 6
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100], key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to insert: ");
+    scanf("%d", &key);
+
+    i = n - 1;
+    while (i >= 0 && arr[i] > key) {
+        arr[i + 1] = arr[i];
+        i--;
+    }
+    arr[i + 1] = key;
+    n++;
+
+    printf("Array after insertion: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
