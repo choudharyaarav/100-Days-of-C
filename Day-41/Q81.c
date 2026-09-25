3435cfe414808ea578b765db7db569cdb23608c4
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:42 +0530

    Day 41: Q81 and Q82 solutions

--- Day-41/Q81.c
+++ Day-41/Q81.c
@@ -1,3 +1,32 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 19/09/26.
+/* Q81: Count characters in a string without using built-in length functions.
+
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+
+Output 2:
+1
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, count = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        count++;
+        i++;
+    }
+
+    printf("Length = %d", count);
+
+    return 0;
+}
--- Day-41/Q82.c
+++ Day-41/Q82.c
@@ -1,3 +1,26 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 19/09/26.
+/* Q82: Print each character of a string on a new line.
+
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
