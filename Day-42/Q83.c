640f8d97fbeb63962a28c9e08a9f48e118c460dc
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:44 +0530

    Day 42: Q83 and Q84 solutions

--- Day-42/Q83.c
+++ Day-42/Q83.c
@@ -1,3 +1,33 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 20/09/26.
+/* Q83: Count vowels and consonants in a string.
+
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+Vowels=2, Consonants=3
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        char ch = str[i];
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            vowels++;
+            } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                consonants++;
+            }
+        i++;
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
--- Day-42/Q84.c
+++ Day-42/Q84.c
@@ -1,3 +1,28 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 20/09/26.
+/* Q84: Convert a lowercase string to uppercase without using built-in functions.
+
+Sample Test Cases:
+Input 1:
+hello
+Output 1:
+HELLO
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 'a' + 'A';
+        i++;
+    }
+
+    printf("Uppercase: %s", str);
+
+    return 0;
+}
