ce4761afd366c3e4e2b4cd4f443c87502566debb
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:55 +0530

    Day 47: Q93 and Q94 solutions

--- Day-47/Q93.c
+++ Day-47/Q93.c
@@ -1,3 +1,49 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 25/09/26.
+/* Q93: Check if two strings are anagrams of each other.
+
+Sample Test Cases:
+Input 1:
+listen
+silent
+Output 1:
+Anagrams
+
+Input 2:
+hello
+world
+Output 2:
+Not anagrams
+*/
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int count[26] = {0};
+    int i = 0, isAnagram = 1;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    for (i = 0; str1[i] != '\0'; i++)
+        count[str1[i] - 'a']++;
+
+    for (i = 0; str2[i] != '\0'; i++)
+        count[str2[i] - 'a']--;
+
+    for (i = 0; i < 26; i++) {
+        if (count[i] != 0) {
+            isAnagram = 0;
+            break;
+        }
+    }
+
+    if (isAnagram)
+        printf("Anagrams");
+    else
+        printf("Not anagrams");
+
+    return 0;
+}
--- Day-47/Q94.c
+++ Day-47/Q94.c
@@ -1,3 +1,44 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 25/09/26.
+/* Q94: Find the longest word in a sentence.
+
+Sample Test Cases:
+Input 1:
+I love programming
+Output 1:
+programming
+*/
+#include <stdio.h>
+
+int main() {
+    char str[200], word[100], longest[100];
+    int i = 0, j, k, maxLen = 0, len;
+
+    printf("Enter a sentence: ");
+    scanf("%[^\n]", str);
+
+    i = 0;
+    while (1) {
+        j = 0;
+        while (str[i] != ' ' && str[i] != '\0') {
+            word[j] = str[i];
+            j++;
+            i++;
+        }
+        word[j] = '\0';
+        len = j;
+
+        if (len > maxLen) {
+            maxLen = len;
+            for (k = 0; k <= len; k++)
+                longest[k] = word[k];
+        }
+
+        if (str[i] == '\0')
+            break;
+        i++;
+    }
+
+    printf("Longest word: %s", longest);
+
+    return 0;
+}
