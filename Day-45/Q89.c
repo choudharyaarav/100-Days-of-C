d2103bce270c73d2e166bcb28e818727afd44f73
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:50 +0530

    Day 45: Q89 and Q90 solutions

--- Day-45/Q89.c
+++ Day-45/Q89.c
@@ -1,3 +1,32 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 23/09/26.
+/* Q89: Count frequency of a given character in a string.
+
+Sample Test Cases:
+Input 1:
+programming
+g
+Output 1:
+2
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100], ch;
+    int i = 0, count = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    printf("Enter character to count: ");
+    scanf(" %c", &ch);
+
+    while (str[i] != '\0') {
+        if (str[i] == ch)
+            count++;
+        i++;
+    }
+
+    printf("Count = %d", count);
+
+    return 0;
+}
--- Day-45/Q90.c
+++ Day-45/Q90.c
@@ -1,3 +1,30 @@
-//
-// Created by Aarav Choudhary on 26/09/26.
-//
+// Created by Aarav Choudhary on 23/09/26.
+/* Q90: Toggle case of each character in a string.
+
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+hELLO
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 'a' + 'A';
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] - 'A' + 'a';
+        i++;
+    }
+
+    printf("Toggled: %s", str);
+
+    return 0;
+}
