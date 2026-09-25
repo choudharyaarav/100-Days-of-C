e14b0e5aef024f74e0c3bc842f12544270fcd1e1
Aarav Choudhary <aaravchoudhary@Aaravs-MacBook-Air.local>
2026-09-26 01:46:21 +0530

    Day 32: Q63 and Q64 solutions

--- Day-32/Q63.c
+++ Day-32/Q63.c
@@ -1,3 +1,37 @@
-//
 // Created by Aarav Choudhary on 10/09/26.
-//
+/* Q63: Merge two arrays.
+
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+*/
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i, arr1[100], arr2[100];
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    printf("Enter %d elements: ", n1);
+    for (i = 0; i < n1; i++)
+        scanf("%d", &arr1[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    printf("Enter %d elements: ", n2);
+    for (i = 0; i < n2; i++)
+        scanf("%d", &arr2[i]);
+
+    printf("Merged array: ");
+    for (i = 0; i < n1; i++)
+        printf("%d ", arr1[i]);
+    for (i = 0; i < n2; i++)
+        printf("%d ", arr2[i]);
+
+    return 0;
+}
--- Day-32/Q64.c
+++ Day-32/Q64.c
@@ -1,3 +1,46 @@
-//
 // Created by Aarav Choudhary on 10/09/26.
-//
+/* Q64: Find the digit that occurs the most times in an integer number.
+
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+*/
+#include <stdio.h>
+
+int main() {
+    long long num;
+    int count[10] = {0}, digit, i, maxCount = 0, maxDigit = 0;
+
+    printf("Enter a number: ");
+    scanf("%lld", &num);
+
+    if (num < 0)
+        num = -num;
+
+    if (num == 0)
+        count[0]++;
+
+    while (num > 0) {
+        digit = num % 10;
+        count[digit]++;
+        num /= 10;
+    }
+
+    for (i = 0; i <= 9; i++) {
+        if (count[i] > maxCount) {
+            maxCount = count[i];
+            maxDigit = i;
+        }
+    }
+
+    printf("Most frequent digit = %d", maxDigit);
+
+    return 0;
+}
